25b0351dab3a9a0da23f24092f66e47c8a4c68f1
chenning <ning.chen@quvideo.com>
2020-03-26 18:04:11 +0800

    [feat]:乞丐版MVP

---
+++ app/src/main/java/com/ning/architecturedemo/MvpView.java
@@ -0,0 +1,35 @@
+package com.ning.architecturedemo;
+
+/**
+ * Created by chenning on 2020/3/26
+ */
+public interface MvpView {
+
+    /**
+     * 显示正在加载进度框
+     */
+    void showLoading();
+
+    /**
+     * 隐藏正在加载进度框
+     */
+    void hideLoading();
+
+    /**
+     * 当数据请求成功后，调用此接口显示数据
+     * @param data 数据源
+     */
+    void showData(String data);
+
+    /**
+     * 当数据请求失败后，调用此接口提示
+     * @param msg 失败原因
+     */
+    void showFailureMessage(String msg);
+
+    /**
+     * 当数据请求异常，调用此接口提示
+     */
+    void showErrorMessage();
+
+}
